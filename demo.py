5f0bf81e406f7b048f2187e0bf2823e13e5d1e42
Callum Atkinson <callum.atkinson@monash.edu>
2026-02-25 15:23:25 +1100

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,3 +16,16 @@ class Dog:
 
     def bark(self, loud: bool = True):
         """*woof*"""
+
+    def doggymaths(x, y):
+        """
+        You can even include tex based mathematical formulas
+
+        Parameters:
+        x: value x
+        y: value y
+
+        Returns:
+        the value of $\\frac{x}{y}$
+        """
+        return x/y
